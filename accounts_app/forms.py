7caa6db0298c4ebb5df9ed4148c72906d190f1e5
capiedrav <capiedrav@gmail.com>
2024-03-03 21:54:49 -0500

    reCaptcha V3 (google) added using django-recaptcha plugin

--- accounts_app/forms.py
+++ accounts_app/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from allauth.account.forms import SignupForm
+from allauth.account.forms import SignupForm, LoginForm
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 from django import forms
 from .utils import get_stores
 from store_app.models import Customer, Address, Store
@@ -10,6 +12,8 @@ class CustomUserCreationForm(UserCreationForm):
     """
     form for the creation of a user.
     """
+
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action="user_creation"))
 
     class Meta:
         model = get_user_model()
@@ -21,17 +25,31 @@ class CustomUserChangeForm(UserChangeForm):
     form for updating user information.
     """
 
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action="user_change"))
+
     class Meta:
         model = get_user_model()
         fields = ("username", "email")
 
 
+class CustomLoginForm(LoginForm):
+    """
+    Form for user log in.
+    """
+
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action="login"))
+
+
 class CustomSignupForm(SignupForm):
+    """
+    Form for user sign up.
+    """
 
     store = forms.ChoiceField(choices=get_stores())
     address = forms.CharField(max_length=50)
     district = forms.CharField(max_length=20)
     phone = forms.CharField(max_length=20)
+    captcha = ReCaptchaField(widget=ReCaptchaV3(action="signup"))
 
     def save(self, request):
 
